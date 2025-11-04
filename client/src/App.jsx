3fb79770e163219acf3d0d0278c0b255eacd51a7
Sah-Rahul <sahfamilyinfo@gmail.com>
2025-11-04 19:58:01 +0545

    setup shadcn

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      
+      <Button>App</Button>
+    </div>
+  )
+}
+
+export default App
